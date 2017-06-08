b58188a69301b685f30bc5aee5ba94a0cf7b79dd
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-07 21:07:13 -0700

    update key_finder to analyze all songs placed in the 'songs' folder

--- key_finder.py
+++ key_finder.py
@@ -1,12 +1,17 @@
 import librosa
 import numpy as np
+import os.path
 import time
 
-print (time.strftime('%I %M %S'))
-y, sr = librosa.load('songs/faded.mp3')
-S = np.abs(librosa.stft(y)**2)
-chroma = librosa.feature.chroma_stft(S=S, sr=sr)
+path = './songs'
+file_names = [f for f in os.listdir(path) if f != '.DS_Store' and f != '.gitkeep' and os.path.isfile(os.path.join(path, f))]
 
-for i in range(len(chroma)):
-    print(sum(chroma[i]))
-print(time.strftime('%I %M %S'))
+for title in file_names:
+    print ("Starting analysis of " + title + " at " + time.strftime('%I:%M:%S'))
+    y, sr = librosa.load('songs/' + title)
+    S = np.abs(librosa.stft(y)**2)
+    chroma = librosa.feature.chroma_stft(S=S, sr=sr)
+
+    for j in range(len(chroma)):
+        print(sum(chroma[j]))
+    print ("Finished analysis of " + title + " at " + time.strftime('%I:%M:%S'))
