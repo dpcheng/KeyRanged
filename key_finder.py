ee0af3bb5da9383d76df3abe84369a41b772c644
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-07 21:51:43 -0700

    refactor script to separate functions

--- key_finder.py
+++ key_finder.py
@@ -5,21 +5,16 @@ import numpy as np
 import os.path
 import time
 
-path = './songs'
-file_names = [f for f in os.listdir(path) if f != '.DS_Store' and f != '.gitkeep' and os.path.isfile(os.path.join(path, f))]
 
-for title in file_names:
-    print ("Starting analysis of " + title + " at " + time.strftime('%I:%M:%S'))
-
+def create_chromagram_sum(title):
     y, sr = librosa.load('songs/' + title)
     S = np.abs(librosa.stft(y)**2)
     chroma = librosa.feature.chroma_stft(S=S, sr=sr)
 
-    chroma_sum = [sum(x) for x in chroma]
-    scale = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+    return [sum(x) for x in chroma]
 
-    for j in range(12):
-        print(scale[j] + ": " + str(chroma_sum[j]))
+
+def display_top_three(chroma_sum, scale):
     chroma_sorted = [x for x in chroma_sum]
     chroma_sorted.sort()
     print("Top three keys are")
@@ -27,4 +22,23 @@ for title in file_names:
         top_sum = chroma_sorted[k]
         print(scale[chroma_sum.index(top_sum)] + ": " + str(top_sum))
 
-    print ("Finished analysis of " + title + " at " + time.strftime('%I:%M:%S'))
+
+def analyze_songs():
+    path = './songs'
+    file_names = [f for f in os.listdir(path) if f != '.DS_Store' and f != '.gitkeep' and os.path.isfile(os.path.join(path, f))]
+
+    for title in file_names:
+        print ("Starting analysis of " + title + " at " + time.strftime('%I:%M:%S'))
+
+        chroma_sum = create_chromagram_sum(title)
+        scale = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+
+        for j in range(12):
+            print(scale[j] + ": " + str(chroma_sum[j]))
+
+        display_top_three(chroma_sum, scale)
+
+        print ("Finished analysis of " + title + " at " + time.strftime('%I:%M:%S'))
+
+
+analyze_songs()
