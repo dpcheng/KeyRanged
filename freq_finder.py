efe78da8fa8c22b6bb0805ed3cd71472dcb36b99
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-16 20:00:00 -0700

    freq_finder now iterates over every song in folder

--- freq_finder.py
+++ freq_finder.py
@@ -10,4 +10,15 @@ def create_spectrogram(title):
     S = np.abs(librosa.stft(y))
     comps, acts = librosa.decompose.decompose(S, n_components=8)
 
-    
+
+def analyze_songs():
+    path = './songs'
+    file_names = [f for f in os.listdir(path) if f != '.DS_Store' and f != '.gitkeep' and os.path.isfile(os.path.join(path, f))]
+    csvfile = open('song_frequencies.csv', 'wb')
+    writer = csv.writer(csvfile, delimiter=",")
+    writer.writerow(["title", "frequencies"])
+
+    for title in file_names:
+        print("Starting analysis of " + title + " at " + time.strftime('%I:%M:%S'))
+
+        spectrogram = create_spectrogram(title)
